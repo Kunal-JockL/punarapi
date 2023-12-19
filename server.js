2661a10a4bf1ba8943af33c2d044bd41f6708b4e
Ian Tauro <sushruth127@gmail.com>
2023-12-20 00:13:58 +0530

    Email working. Sends mail to client and owners

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 require("dotenv").config();
 const SRCEMAIL = process.env.EMAIL;
 const SRCEMAILPWD = process.env.PWD;
+const DESTEMAILS = process.env.DEST.split(' ');
 
 
 const app = express();
@@ -38,7 +39,7 @@ app.post('/request_sample', (req, res) => {
     phoneNumber = formBody.phno,
     jobPosition = formBody.position,
     sampleTile = formBody.sample,
-    company = formBody.company,
+    colors = formBody.colors,
     address = formBody.address,
     city = formBody.city,
     state = formBody.state,
@@ -52,23 +53,41 @@ app.post('/request_sample', (req, res) => {
           pass: SRCEMAILPWD,
         },
     });
-    
-    const mailOptions = {
+
+    // SEND MAIL TO CUSTOMER
+    const customerMailOptions = {
         from: SRCEMAIL,
         to: email,
-        subject: 'Test for Punarapi',
-        text: `Just a test!`,
+        subject: `Punarapi - Your sample request for ${sampleTile} tiles has been received!`,
+        text: `Hello ${firstName + ' ' + lastName},\n\nThank you for requesting a sample for ${sampleTile} tiles. We shall contact you shortly for any further details.`,
     };
     
-    transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(customerMailOptions, (error, info) => {
         if (error) {
           return console.error(error);
         }
         console.log('Email sent:', info.response);
-        res.send('Form submitted successfully!');
     });
 
-    res.send('Form not sent!');
+    // SEND MAIL TO OWNERS
+
+    for(let i = 0; i < DESTEMAILS.length; i++) {
+        const mailOptions = {
+            from: SRCEMAIL,
+            to: DESTEMAILS[i],
+            subject: `Punarapi - A sample request for ${sampleTile} tiles has been arrived!`,
+            text: `Form Details:\nFirst Name: ${firstName}\nLast Name: ${lastName}\nE-Mail:\n ${email}\nPhone No: ${phoneNumber}\nPosition: ${jobPosition}\nRequested Sample: ${sampleTile} Tiles\nColors: ${colors}\nAddress: ${address}\nCity: ${city}\nState: ${state}\nZIP Code: ${zipCode}\nCountry: ${country}\n\nContact the client through email as soon as possible.`,
+        }
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              return console.error(error);
+            }
+            console.log('Email sent:', info.response);
+        });
+    }
+    
+    res.send('Form submitted successfully!');
 })
 
 app.listen(3000, () => {
