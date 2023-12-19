df35ad791bdce3906827e54263724a6a6d559569
Ian Tauro <sushruth127@gmail.com>
2023-12-19 23:54:01 +0530

    added .env

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+
+require("dotenv").config();
+const SRCEMAIL = process.env.EMAIL;
+const SRCEMAILPWD = process.env.PWD;
+
+
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/', (req, res) => {
     res.render('punarapi');
@@ -36,7 +45,30 @@ app.post('/request_sample', (req, res) => {
     zipCode = formBody.zip,
     country = formBody.country;
 
-    res.send('Request sent');
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: SRCEMAIL,
+          pass: SRCEMAILPWD,
+        },
+    });
+    
+    const mailOptions = {
+        from: SRCEMAIL,
+        to: email,
+        subject: 'Test for Punarapi',
+        text: `Just a test!`,
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.error(error);
+        }
+        console.log('Email sent:', info.response);
+        res.send('Form submitted successfully!');
+    });
+
+    res.send('Form not sent!');
 })
 
 app.listen(3000, () => {
